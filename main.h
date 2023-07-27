63ca3aeca5416ad8fd77a12a85cba2489c3d8623
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-07-27 09:56:54 +0100

    main.h

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * struct format - the format components
@@ -15,12 +16,11 @@ typedef struct format
 	char c;
 	int (*f)(va_list);
 } format_t;
-#include <unistd.h>
 int _printf(const char *format, ...);
 int print_char(va_list);
 int print_string(va_list);
 int print_int(va_list);
-int conv(va_list, format_t *, const char *);
+int handle(va_list, format_t *, const char *);
 int _putchar(char);
 int print_num(int);
 int print_percent(__attribute__ ((unused)) va_list);
