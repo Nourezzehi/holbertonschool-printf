6286d998276b0bb58087ca7e127dd7d5c4643d4b
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-07-27 12:16:31 +0100

    last

--- print_func.c
+++ print_func.c
@@ -24,10 +24,10 @@ int print_string(va_list args)
 {
 	char *s;
 	int i = 0;
-	
+
 	s = va_arg(args, char *);
 	if (!s)
-		s = ("(null)");
+		s = ("null");
 	while (*(s + i))
 	{
 		_putchar(*(s + i));
