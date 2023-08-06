8abd17c45eed7174420b417fa3e53dd0aa9481e1
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-06 22:13:16 +0100

    s.c

--- main.h
+++ main.h
@@ -22,10 +22,8 @@ int print_int(va_list);
 int _printf(const char *format, ...);
 int (*get_func(const char ch))(va_list);
 int _puts(char *);
-int print_binary(va_list);
 int _putchar(char);
 int print_decimal(va_list);
-char *convert(unsigned long int, int, int);
 int print_percent(__attribute__ ((unused)) va_list);
 
 #endif
